e9342ac5d76b8ddfeeba9d47a40c1d5c507b1ef8
srisathyasidmec <srisathya@sidmectech.in>
2025-05-08 13:14:46 +0530

    saved

--- solarhub/__manifest__.py
+++ solarhub/__manifest__.py
@@ -2,18 +2,12 @@
     'name':'solar hub management',
     'version':'18.0',
     'author':"krisnaveni and sri sathya",
-<<<<<<< HEAD
-    'depends':['hr',],
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/solaremployee.xml',
-=======
-    'depends':['contacts','mail',],
+    'depends':['hr','contacts','mail',],
     'data': [
         'security/ir.model.access.csv',
+        'views/solaremployee.xml',
         'views/view_customer.xml',
         'views/solarpanel.xml',
->>>>>>> 8261fee1ba33bb1113d54d9f797264ccd4061538
         'views/menu.xml',
     ],
 }
