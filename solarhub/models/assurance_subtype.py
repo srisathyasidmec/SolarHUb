338cb55d1bda0341e90c9993a4d7cbebfc700aee
srisathyasidmec <srisathya@sidmectech.in>
2025-05-12 10:03:54 +0530

    saved

--- solarhub/models/assurance_subtype.py
+++ solarhub/models/assurance_subtype.py
@@ -4,6 +4,7 @@ from odoo import models,fields,api
 class SystemProperty(models.Model):
     _name="assurance.subtype"
     _description = "system assurance subtype"
+    _rec_name = "assurance_subtype"
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
     assurance = fields.Many2one("system.assurance", "Assurance Type")
--- solarhub/models/service_type.py
+++ solarhub/models/service_type.py
@@ -4,6 +4,7 @@ from odoo import models,fields,api
 class SystemService(models.Model):
     _name="system.service"
     _description = "system service type"
+    _rec_name = "title"
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
     title=fields.Char("Title")
